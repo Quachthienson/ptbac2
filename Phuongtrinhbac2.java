f903f3a610565ac0920480d5b5d66071a08d4b48
Quách Phạm Thiện Sơn <97727933+Quachthienson@users.noreply.github.com>
2022-02-25 16:09:55 +0700

    Add files via upload

---
+++ Phuongtrinhbac2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai6java;
+
+/**
+ *
+ * @author Lab
+ */
+class Phuongtrinhbac2 {
+
+    void nhapPT() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void giaiPt() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
